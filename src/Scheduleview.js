72a5a75741991c10455b4ba5b058381de0a23895
amgoodfellow <amgoodfellow@oakland.edu>
2017-05-23 16:35:50 -0400

    Finalized look of schedule view

--- src/Scheduleview.js
+++ src/Scheduleview.js
@@ -7,7 +7,6 @@ import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
 
 
-
 class Scheduleview extends Component {
 
   generateDayCards(){
@@ -32,18 +31,16 @@ class DayCard extends Component {
   render(){
     return (
       <div>
-        <Card>
-          <CardContent>
+        <Card style={{marginTop: 9}}>
+          <CardContent style={{paddingBottom: 0}}>
             <Typography type="headline" component="h2">
               {this.props.dayName} 
             </Typography>
+            <Divider/>
             <Typography component="div">
               <List>
-                <ListItem button>
-                  <ListItemText primary="Inbox" />
-                </ListItem>
-                <ListItem button>
-                  <ListItemText primary="Drafts" />
+                <ListItem style={{padding: 0}}>
+                  <ListContent title="CSE 280" name="Sophomore Project" startTime="10:20" endTime="12:30" buildingRoom="EC 321"/>
                 </ListItem>
               </List>
             </Typography>
@@ -53,6 +50,30 @@ class DayCard extends Component {
     );
   }
 }
+
+
+const ListContent = ({title, name, startTime, endTime, buildingRoom}) => {
+  console.log(title)
+  return (
+    <div style={{width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'space-around', fontSize: 16}} >
+      <div>
+        <div style={{fontSize: '.83em', fontWeight: 'bold'}}>Course Name</div>
+        <p>{name}</p>
+      </div>
+      <div style={{border:"1px solid lightgray"}}></div>
+      <div>
+        <div style={{fontSize: '.83em', fontWeight: 'bold'}}>Meeting Time</div>
+        <p>{`${startTime} - ${endTime}`}</p>
+      </div>
+      <div style={{border:"1px solid lightgray"}}></div>
+      <div>
+        <div style={{fontSize: '.83em', fontWeight: 'bold'}}>Location</div>
+        <p>{buildingRoom}</p>
+      </div>
+    </div> 
+  )
+}
+
 
 
 
