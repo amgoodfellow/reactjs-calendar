5a8d54f1cc20973e1803d6fdc3f551c8183b9a95
amgoodfellow <amgoodfellow@oakland.edu>
2017-06-15 09:53:12 -0400

    Meetings are being displayed correctly #5

--- src/Weekview.js
+++ src/Weekview.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import { prettyHours, dayNames, shortDayNames } from "./utils/Strings"
-import {getStartOfWeek, getWeekArray} from "./utils/DateHelper"
+import {getStartOfWeek, getWeekArray, getMilitaryTime} from "./utils/DateHelper"
 
 const columnStyle = {
   width: "14%",
@@ -33,18 +33,31 @@ const weekCol = (meetings) => {
   let column = []
   for (let i = 0; i < 34; i++) {
     //if there is a meeting happening today (the math is necessary since the weekview isn't 24 hours)
-    if (meetings !== null && meetings !== undefined && (meetings[0].starttime.substring(0, 2) * 2 -14) === i){
-      
-      column.push(
-        <div key={"weekCol" + i} style={{ border: "1px solid lightgrey", height: "2.65%" }}>
-          <button style={{backgroundColor: 'lightblue', border: 'none', width: '100%', height: '250%'}}>
-            {meetings[0].coursetitle}
-          </button>
-        </div>
-      )
+    if (meetings !== null && meetings !== undefined){
+      let shouldPushClass = false
+      let meetingdata = ""
+      for (let j = 0; j < meetings.length; j++){
+        if ((getMilitaryTime(meetings[j].starttime).hours * 2) -14 == i){
+          shouldPushClass = true
+          meetingdata = meetings[j]
+        }
+      }
+      if (shouldPushClass === true){
+        column.push(
+          <div style={{ border: "1px solid lightgrey", height: "2.65%" }}>
+            <button style={{backgroundColor: '#0074b7', color: 'white', fontWeight: 'bold', border: 'none', width: '100%', height: '250%'}}>
+              {meetingdata.coursetitle}
+            </button>
+          </div>
+        )
+      }else{
+        column.push(
+              <div style={{ border: "1px solid lightgrey", height: "2.65%" }}></div>
+              )
+      }
     }else {
       column.push(
-          <div key={"weekCol" + i} style={{ border: "1px solid lightgrey", height: "2.65%" }}/>
+          <div style={{ border: "1px solid lightgrey", height: "2.65%" }}></div>
       )
     }
   }
@@ -61,18 +74,18 @@ class Weekview extends Component {
     const startOfWeek = getStartOfWeek(currentDate.month, currentDate.year, currentDate.week)
     let weekcols = []
     for (let i = 0; i < 7; i++){
-      let a = ""
+      let weekGrid = ""
       try {
-        a = weekCol(this.props.meetings[currentDate.month][startOfWeek + i])
+        weekGrid = weekCol(this.props.meetings[currentDate.month][startOfWeek + i])
       }catch(err){
-        a = weekCol(null)
+        weekGrid = weekCol(null)
       }
       weekcols.push(
         <div style={columnStyle}>
           <div style={{ height: "2.7%" }}>
             {dayNames[i]}
           </div>
-            {a}
+            {weekGrid}
         </div>
       )
     }
@@ -82,7 +95,6 @@ class Weekview extends Component {
   render() {
     const currentDate = this.props.currentDateRange
     const startOfWeek = getStartOfWeek(currentDate.month, currentDate.year, currentDate.week)
-    console.log(getWeekArray(currentDate.month, currentDate.year, currentDate.week))
     return (
       <div style={{ border: "1px solid black", height: 950, display: "flex", fontFamily: "Arimo" }}>
         <div style={hourColStyle}>
