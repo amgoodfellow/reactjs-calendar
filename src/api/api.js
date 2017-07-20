cb29382d6a0572c0ef5df66831b5b232169831eb
amgoodfellow <amgoodfellow@oakland.edu>
2017-07-20 10:35:25 -0400

    Included credentials for api to help with uportal cookie checks

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 export const getEvents = async url => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, { credentials: "include" })
     const events = await response.json()
     return events
   } catch (err) {
