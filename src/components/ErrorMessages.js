f8cdf9000984f547e42eb17782c7639a127e0078
dorisgjata <dorisgjata@oakland.edu>
2017-07-21 10:01:26 -0400

    formated the code

--- src/components/ErrorMessages.js
+++ src/components/ErrorMessages.js
@@ -4,7 +4,7 @@ import Typography from "material-ui/Typography";
 import Card, { CardHeader, CardContent, CardMedia } from "material-ui/Card";
 import { withStyles, createStyleSheet } from "material-ui/styles";
 import PropTypes from "prop-types";
-import grizzHead from "/home/dorisgjata/react-calendar-2.0/src/utils/grizzHead.png"
+import grizzHead from "/home/dorisgjata/react-calendar-2.0/src/utils/grizzHead.png";
 
 const styleSheet = createStyleSheet("ErrorMessages", theme => ({
   root: {
@@ -14,21 +14,30 @@ const styleSheet = createStyleSheet("ErrorMessages", theme => ({
   },
   card: {
     backgroundColor: "#fafafa ",
-  
+
     borderLeftStyle: "solid",
     borderLeftWidth: "12px",
-    borderLeftColor: "#d32f2f",
-  
+    borderLeftColor: "#d32f2f"
   },
-    media: {
+  media: {
     padding: "10px",
     display: "flex",
     alignSelf: "center",
     marginLeft: "10px"
 
     //backgroundColor: "#0074b7"
+  },
+  main: {
+    display: "flex",
+    flexDirection: "row"
+  },
+  head: {
+    display: "flex",
+    flexFlow: "column wrap"
+  },
+  content:{
+    paddingTop: 0 
   }
-  
 }));
 class ErrorMessages extends Component {
   render() {
@@ -36,17 +45,23 @@ class ErrorMessages extends Component {
     return (
       <div className={classes.root}>
         <Card className={classes.card}>
-           <div style={{display: "flex", flexDirection: "row"}}>
-          <CardMedia className={classes.media}>
-            <img src={grizzHead} width="123.6px" height="95.6px" alt="Grizz Head Image" aria-hidden="true"/>
-        </CardMedia>
-       <div  style={{display: "flex", flexFlow: "column wrap"}}>
-          <CardHeader title="We were unable to fetch data at this time" />
+          <div className={classes.main}>
+            <CardMedia className={classes.media}>
+              <img
+                src={grizzHead}
+                width="123.6px"
+                height="95.6px"
+                alt="Grizz Head Image"
+                aria-hidden="true"
+              />
+            </CardMedia>
+            <div className={classes.head}>
+              <CardHeader title="We were unable to fetch data at this time" />
 
-          <CardContent>
-            <Typography type="body1">Please try again later</Typography>
-          </CardContent>
-          </div>
+              <CardContent className={classes.content}>
+                <Typography type="body1">Please try again later</Typography>
+              </CardContent>
+            </div>
           </div>
         </Card>
       </div>
